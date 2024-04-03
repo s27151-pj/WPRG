fcc7f7dd9a983af764ee2f2beab96cb9cd7f5e1c
s27151-pj <115358243+s27151-pj@users.noreply.github.com>
2024-04-03 14:30:20 +0200

    Update zad3.php

--- zjazd1/zad3.php
+++ zjazd1/zad3.php
@@ -1,10 +1,25 @@
 <?php
-function fibonacciSequence($number) {
-    $fibonacciArrey = array();
-    while count($fibonacciArrey) < $number;
-    	$len = count($fibonacciArrey);
-    	$newElement = $fibonacciArrey[$len-2] + $fibonacciArrey[$len-1]
-        array_push($fibonacciArrey, $newElement);
-        
-    fibonacciSequence(10)
+
+function fibonacci($n) {
+    $sequence = [0, 1];
+    for ($i = 2; $i <= $n; $i++) {
+        $sequence[$i] = $sequence[$i - 1] + $sequence[$i - 2];
+    }
+    return $sequence;
+}
+
+function printOdd($sequence) {
+    $number = 1;
+    foreach ($sequence as $element) {
+        if ($element % 2 != 0) {
+            echo $number . "; " . $element . "\n";
+            $number++;
+        }
+    }
+}
+
+$n = 10;
+$sequence = fibonacci($n);
+printOdd($sequence);
+
 ?>
